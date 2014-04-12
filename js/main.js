515d6016d79c33169ea1886a9afaf8d60f9e315e
Roman Kierzkowski <roman.kierzkowski@gmail.com>
2014-04-12 14:17:15 +0200

    CNAME

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 function switchMenu(page) {
 	$(".nav.navbar-nav li").removeClass("active");
-    $(".nav.navbar-nav li a[href='" + page + "']").parent().addClass("active");
+	if (page.indexOf("\/blog") == 0) {
+		$(".nav.navbar-nav li a[href='/blog/']").parent().addClass("active");
+	} else {
+		$(".nav.navbar-nav li a[href='" + page + "']").parent().addClass("active");	
+	}
+    console.log(page);
 }
